e222e2fc672212f76674dd42e6c505a9f568aff5
Sarvang Dave <sarvang00@gmail.com>
2025-06-10 14:16:53 -0300

    Adding basic components

---
+++ 1_basic_concepts/main.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+type Ingridient struct {
+	Name     string
+	Calories float64
+}
+
+// Checks if an ingridient is High Calorie.
+// For each ingridient, the bar is at 70, to check if it is High Calorie.
+func (i Ingridient) isHighCalorie() bool {
+	return i.Calories > 70
+}
+
+type Recipie struct {
+	Name                string
+	IngridientsQuantity map[Ingridient]int
+	totalCalories       float64
+}
+
+func (r Recipie) isHighCalorie() bool {
+	return r.totalCalories > 680
+}
+
+func (r Recipie) displayRecipie() {
+	fmt.Println("Ingridient\tQuantity")
+	fmt.Println("------------\t------")
+	for ing, quant := range r.IngridientsQuantity {
+		fmt.Printf("%v\t%v\n", ing.Name, quant)
+	}
+	fmt.Println("--------------------")
+	fmt.Println("Total Calories: ", r.totalCalories)
+}
+
+// Calculates total calories for a Recipie
+func (r *Recipie) TalculatetotalCalories() {
+	var totalCals float64 = 0
+
+	for ing, ingQuant := range r.IngridientsQuantity {
+		totalCals += ing.Calories * float64(ingQuant)
+	}
+
+	r.totalCalories = totalCals
+}
+
+func main() {
+	ingridient1 := Ingridient{
+		Name:     "Pepper",
+		Calories: 8,
+	}
+
+	ingridient2 := Ingridient{
+		Name:     "Pepper Mozza Sauce",
+		Calories: 85,
+	}
+
+	// fmt.Println(ingridient1)
+	// fmt.Println(ingridient1.isHighCalorie())
+
+	// fmt.Println(ingridient2)
+	// fmt.Println(ingridient2.isHighCalorie())
+
+	recpie1 := Recipie{
+		IngridientsQuantity: make(map[Ingridient]int),
+		totalCalories:       0,
+	}
+
+	recpie1.IngridientsQuantity[ingridient1] = 2
+	recpie1.IngridientsQuantity[ingridient2] = 1
+	recpie1.TalculatetotalCalories()
+
+	recpie1.displayRecipie()
+	fmt.Println(recpie1.isHighCalorie())
+}
